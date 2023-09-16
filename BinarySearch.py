710c1849b48d395c5b43c731dd1e2cc806f89589
theLoneCheetah <105351733+theLoneCheetah@users.noreply.github.com>
2023-09-16 22:50:07 +0300

    Add files via upload

---
+++ BinarySearch.py
@@ -0,0 +1,19 @@
+def binary_search(array, begin_index, end_index, key, step):
+    if array[end_index] == key:
+        return step
+    if begin_index > end_index:
+        return -1
+    middle = (begin_index + end_index) // 2
+    #print(begin_index, end_index, middle)
+    if array[middle] == key:
+        return step
+    elif array[middle] < key:
+        return binary_search(array, middle + 1, end_index, key, step + 1)
+    else:
+        return binary_search(array, begin_index, middle - 1, key, step + 1)
+
+size = int(input("Введите размер массива: "))
+array = [a + 1 for a in range(size)]
+#array = [1, 2, 3, 4, 19, 30, 31, 33]
+key = int(input("Введите искомый элемент: "))
+print(binary_search(array, 0, len(array)  - 1, key, 1))
